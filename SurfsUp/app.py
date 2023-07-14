fae277ed787ed5e4fb96369faa9d454397dcca23
ralfwelvers <ralf.welvers@gmail.com>
2023-07-13 20:51:35 -0500

    started app.py build

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,5 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
 
 
 #################################################
@@ -22,9 +22,32 @@
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+@app.route("/about")
+def about():
+    name = "Peleke"
+    location = "Tien Shan"
+
+    return f"My name is {name}, and I live in {location}."
+
+
+@app.route("/contact")
+def contact():
+    email = "peleke@example.com"
+
+    return f"Questions? Comments? Complaints? Shoot an email to {email}."
+
+
+# 4. Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)
