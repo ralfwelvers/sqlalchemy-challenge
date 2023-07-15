ab10913699b1308cb3fa56648030f81c4cbd2820
ralfwelvers <ralf.welvers@gmail.com>
2023-07-15 16:23:45 -0500

    finished first route

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,7 @@
 # Import the dependencies.
 import numpy as np
+import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -8,28 +10,32 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import pandas as pd
+
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
@@ -39,27 +45,59 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/><br/>"
-        f"<a href=""/api/v1.0/about"">About</a><br/>"
-        f"/api/v1.0/contact"
+        f"<a href=""/api/v1.0/precipitation"">precipitation</a><br/>"
+        f"<a href=""/api/v1.0/stations"">stations</a><br/>"
+        f"<a href=""/api/v1.0/tobs"">tobs</a><br/>"
+        f"<br/>"
+        f"<a href=""/api/v1.0/contact"">contact</a><br/>"
+        
     )
 
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return "Hello. Welcome to Ralf's Hawaii weather API. Details on the routes are below."
 
 
-@app.route("/api/v1.0/about")
-def about():
-    name = "Peleke"
-    location = "Tien Shan"
+@app.route("/api/v1.0/precipitation")
+def precipitation():
 
-    return f"My name is {name}, and I live in {location}."
+    # date format for query
+    format = '%Y-%m-%d'
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Starting from the most recent data point in the database. 
+    # Calculate the date one year from the last date in data set.
+    # Perform a query to retrieve the data and precipitation scores
+    # Save the query results as a Pandas DataFrame.
+    df = pd.read_sql_query(
+        session.query(measurement.date, measurement.prcp).filter(measurement.date >= 
+        dt.datetime.strptime(session.query(measurement).order_by(measurement.date.desc()).first().date, format) - relativedelta(years = 1)).statement
+        ,con=engine)
+    
+    prcp_dict = df.set_index('date').to_dict()['prcp']
+
+    return jsonify(prcp_dict)
 
 
-@app.route("/contact")
+@app.route("/api/v1.0/stations")
+def stations():
+
+
+
+    return f"pending."
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+
+
+    return f"pending."
+
+
+@app.route("/api/v1.0/contact")
 def contact():
-    email = "peleke@example.com"
+    email = "ralf.welvers@gmail.com"
 
     return f"Questions? Comments? Complaints? Shoot an email to {email}."
 
@@ -67,3 +105,6 @@ def contact():
 # 4. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Close Session
+session.close()
