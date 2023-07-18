a0bdb09b39ddbb90a6aba5a9859c0607dbe9e87f
ralfwelvers <ralf.welvers@gmail.com>
2023-07-17 19:28:51 -0500

    completed flask api

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,16 +1,19 @@
 # Import the dependencies.
 import numpy as np
+import pandas as pd
 import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, text, func
 
 from flask import Flask, jsonify
+import json
 
-import pandas as pd
+# date format for query
+format = '%Y-%m-%d'
 
 #################################################
 # Database Setup
@@ -40,32 +43,82 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# home page
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
+        f"Hello. Welcome to Ralf's Hawaii weather API. Details on the routes are below.<br/><br/>"
         f"Available Routes:<br/><br/>"
-        f"<a href=""/api/v1.0/precipitation"">precipitation</a><br/>"
-        f"<a href=""/api/v1.0/stations"">stations</a><br/>"
+        f"The <b>precipitation</b> route returns the last 12 months of precipitation data:<br/><br/>"
+        f"<a href=""/api/v1.0/precipitation"">precipitation</a><br/><br/>"
+        f"The <b>stations</b> route returns the complete list of stations:<br/><br/>"
+        f"<a href=""/api/v1.0/stations"">stations</a><br/><br/>"
+        f"The temperature observations <b>(tobs)</b> route returns the dates and temperature observations of the most-active station for the previous year of data:<br/><br/>"
         f"<a href=""/api/v1.0/tobs"">tobs</a><br/>"
         f"<br/>"
+        f"There is also a route where you can pass in a <b>start date</b> and retrieve the minimum temperature, the average temperature, and the maximum temperature from the provided start date \
+        to the most recent date available.<br/>"
+        f"Here's the path and format for this route:<br/><br/>"
+        f"/api/v1.0/&lt;start&gt;<br/><br/>The start date should be in the following format: YYYY-MM-DD"
+        f" Here's an example for this route:<br/><br/>"
+        f"/api/v1.0/2016-08-08<br/><br/>"
+        f"There is also a route where you can pass in a <b>start date</b> and an <b>end date</b> to retrieve the minimum temperature, the average temperature, and the maximum temperature from the provided start date \
+        to provided end date.<br/>"
+        f"Here's the path and format for this route:<br/><br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/><br/>The start date and end date should be in the following format: YYYY-MM-DD"
+        f" Here's an example for this route:<br/><br/>"
+        f"/api/v1.0/2014-08-08/2016-08-08<br/><br/>"
         f"<a href=""/api/v1.0/contact"">contact</a><br/>"
         
     )
 
+# returns temperature data from a provided start date
+@app.route("/api/v1.0/<start>")
+def temp_startdate(start):
 
-@app.route("/")
-def index():
-    return "Hello. Welcome to Ralf's Hawaii weather API. Details on the routes are below."
+    df2 = pd.read_sql_query(
+    "select measurement.tobs from \
+    measurement where measurement.date >= '" + str(start) + "'"
+    ,con=engine)
+
+    data = {
+        "minimum temperature" : df2['tobs'].min(axis=0),
+        "average temperature" : df2['tobs'].mean(axis=0),
+        "maximum temperature" : df2['tobs'].max(axis=0),
+    }
+
+    if df2.empty:
+        return f"No data returned."
+    else:
+        return jsonify(data)
+
+# returns temperature data from a provided start date and end date
+@app.route("/api/v1.0/<start>/<end>")
+def temp_startdate_enddate(start,end):
+
+    df2 = pd.read_sql_query(
+    "select measurement.tobs from \
+    measurement where measurement.date between '" + str(start) + "' and '" + str(end) + "'"
+    ,con=engine)
+
+    data = {
+        "minimum temperature" : df2['tobs'].min(axis=0),
+        "average temperature" : df2['tobs'].mean(axis=0),
+        "maximum temperature" : df2['tobs'].max(axis=0),
+    }
+
+    if df2.empty:
+        return f"No data returned."
+    else:
+        return jsonify(data)
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-    # date format for query
-    format = '%Y-%m-%d'
-
-    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Design a query to retrieve the last 12 months of precipitation data. 
     # Starting from the most recent data point in the database. 
     # Calculate the date one year from the last date in data set.
     # Perform a query to retrieve the data and precipitation scores
@@ -83,16 +136,45 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
 
+    # SQL query using groupby and count function
+    query = sqlalchemy.select([
+        measurement.station
+    ]).distinct()
+    
+    # get all the records
+    result = engine.execute(query).fetchall()
+    
+    return jsonify({'stations': [dict(row) for row in result]})
 
-
-    return f"pending."
 
 @app.route("/api/v1.0/tobs")
 def tobs():
 
+    # SQL query using groupby and count function
+    query = sqlalchemy.select([
+        measurement.station,
+        sqlalchemy.func.count(measurement.station)
+    ]).group_by(measurement.station).order_by(sqlalchemy.func.count(measurement.station).desc())
+    
+    # get all the records
+    result = engine.execute(query).fetchall()
+    
+    station_high = result[0][0]
 
+    query2 = " select measurement.date from \
+    measurement order by measurement.date desc limit 1"
 
-    return f"pending."
+    print(query2)
+    result2 = engine.execute(query2).fetchall()
+
+    date_filter = dt.datetime.strptime(result2[0][0],format) - relativedelta(years = 1)
+
+    query3 = "select measurement.date, measurement.tobs from \
+    measurement where station = '" + station_high + "'" + " and measurement.date >= '" + str(date_filter) + "'"
+
+    result3 = engine.execute(query3).fetchall()
+
+    return jsonify({'date and tobs': [dict(row) for row in result3]})
 
 
 @app.route("/api/v1.0/contact")
